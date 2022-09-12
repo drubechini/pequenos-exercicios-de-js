7dbece1ba935ba2c845933a900ba3a56ea988a51
Deborah Rubechini <drubechini@gmail.com>
2022-09-12 09:38:13 -0300

    adiciona pastas botao e conversor

---
+++ botao trocando tema/index.js
@@ -0,0 +1,9 @@
+function temaEscuro(){
+    document.getElementById('body').classList.add('escuro');
+    
+}
+
+function temaClaro(){
+    document.getElementById('body').classList.remove('escuro');
+    
+}
---
+++ conversor graus/index.js
@@ -0,0 +1,19 @@
+
+let celsius = +prompt("Digite um número que represente a temperatura em Celsius. (Exemplo: 25)");
+
+let fahrenheit = 9/5 + 32
+
+function converter(){
+ temperatura = celsius + fahrenheit
+console.log(temperatura)
+
+document.getElementById("temperatura").innerHTML = `A temperatura em Fahrenheit é: ${temperatura}.`
+
+}
+
+converter()
+
+
+
+
+
